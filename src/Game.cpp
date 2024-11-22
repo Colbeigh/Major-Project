52afef5ce3aaf928f045a19c2ee12f3db546d3ec
hanc2720 <colby.hanna@uleth.ca>
2024-11-21 20:12:12 -0700

    Created Change Environment function

---
+++ src/Game.cpp
@@ -0,0 +1,37 @@
+#include <Game.hpp>
+
+
+
+class Game{ 
+ public:
+  Game(){
+    currentEnvironment = Environment(environments.front()) 
+  }
+  virtual ~Game(){
+    delete currentEnvironment;
+  }
+  void Start(){
+  }
+  bool isrunning(){
+
+  }
+  void changeEnvironment(){
+    if (currentEnvironment != nullptr) {
+      delete currentEnvironment;
+      currentEnvironment = nullptr;
+    }
+    environments.pop_front();
+    currentEnvironment = Environment(environments.front())
+  }
+
+
+
+
+
+  player Player;
+  currentEnvironment* = nullptr;
+  std::list<std::string> environments{"Passenger", "Dining", "Gambling",
+  "Luggage", "Baggage", "Between", "Prison", "Medical", "Armory", "Engine"}
+};
+
+#endif
