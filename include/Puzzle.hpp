893134b597c5a40ebf3946dd85b7c2915288b644
pinj2720 <j.pineda@uleth.ca>
2024-11-22 09:44:59 -0700

    Added a text file for start puzzles and updated puzzle.hpp

--- include/Puzzle.hpp
+++ include/Puzzle.hpp
@@ -5,42 +5,70 @@
 
 #include <iostream> 
 #include <fstream>
+#include <string>
 
 #ifndef PUZZLE_HPP_INCLUDED
 #define PUZZLE_HPP_INCLUDED
 
-class Puzzle{
+class Puzzle {
  public:
-  Puzzle();
-  virtual ~Puzzle();
-  virtual void startPuzzle();
-  virtual void failPuzzle();
-  virtual void solution();
-  virtual int giveReward();
-  bool solved();
-  virtual int getID(int puzzleID) = 0;
+  Puzzle() {
+    std::cout << "hi\n";
+  }
+  virtual ~Puzzle() {
+    std::cout <<"bye\n";
+  }
+  virtual void startPuzzle() =0;
+  virtual void failPuzzle() = 0;
+  virtual void solution() = 0;
+  virtual int giveReward() = 0;
+  
 };
 
 #endif //Puzzle_hpp_included
 
 
-class ticketPuzzle: public Puzzle{
+class ticketPuzzle: public Puzzle {
 public:
 
-void startPuzzle(){
-std::ifstream file;
-std::string filename;
-int puzzleText;
+void startPuzzle() override {
+  //open file
+  std::ifstream file ("beginPuz.txt");
+  if(file.fail()) {
+    std::cout <<"file did not open\n";
 
+  } else {
+      std::string paragraph;
+      while (std::getline(file, paragraph, '$')) {
+        if (paragraph.find ("II.") == 0){
+        std::cout << paragraph << '\n';
+        }  
+        else { break; }
+      }
+
+    }
+  file.close();
+
+}
+
+void failPuzzle() {
+
+}
+void solution() {
+
+}
+int giveReward() {
+
+}
+bool solved() {
+
+}
 
 };
-void failPuzzle();
-void solution();
-int giveReward();
-bool solved();
 
-int getId(int puzzleID){
-  return puzzleID = 1;
-};
+int main() {
 
-};
+ticketPuzzle first; 
+first.startPuzzle();
+  return 0;
+}
