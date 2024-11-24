e4e2cdce125be9fd98c0358f836745ae8fb31f5b
Jr <jratm32@gmail.com>
2024-11-23 17:07:02 -0800

    added base NPC functionality

--- include/NPC.hpp
+++ include/NPC.hpp
@@ -3,12 +3,24 @@
  * @date Fall 2024
  */
 #include <iostream>
+#include <string>
+#include <map>
 #ifndef NPC_HPP_INCLUDED
 #define NPC_HPP_INCLUDED
 class NPC {
  public:
+  enum dialogueOptions {HAPPY, SAD, ANGRY}; //enum for our default dialogue
   NPC(); //default constructor
+  NPC(const std::string NPCname); //name constructor
   virtual ~NPC(); //destructor
+  std::string getName();
+  void initializeDialogueOptions();
+  void displayDialogue(dialogueOptions option);
+
+ private:
+  std::string name;
+  //map with dialogueOptions key and string value
+  std::map<dialogueOptions, std::string> dialogue;
 };
 
 #endif
--- src/NPC.cpp
+++ src/NPC.cpp
@@ -3,6 +3,27 @@
  * @date Fall 2024
  */
 #include "NPC.hpp"
+#include <iostream>
+#include <string>
 
-NPC::NPC() {}
+NPC::NPC() {
+    name = "Default NPC";
+    initializeDialogueOptions();
+}
+
+NPC::NPC(const std::string NPCname) {
+    name = NPCname;
+    initializeDialogueOptions();
+}
+
 NPC::~NPC() {}
+
+std::string NPC::getName() {
+    return name;
+}
+
+void NPC::initializeDialogueOptions() {
+    dialogue[HAPPY] = "This dialogue is happy";
+    dialogue[SAD] = "This dialogue is sad";
+    dialogue[ANGRY] = "This dialogue is angry";
+}
