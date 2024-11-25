a3495f82855829481dfb8b94bc59ce50c1de3615
hanc2720 <colby.hanna@uleth.ca>
2024-11-24 23:23:24 -0700

    Created Factory Item Class

---
+++ src/FactoryItem.cpp
@@ -0,0 +1,12 @@
+ #include "FactoryPuzzle.hpp"
+
+ Puzzle* FactoryPuzzle::createPuzzle(const std::string& type){
+    if (type == "Ticket"){
+        return new Ticket();
+    } else if (type == "PunchedTicket") {
+        return new PunchedTicket();
+    } else if (type == "Quarter") {
+        return new Quarter(); 
+    } else {
+        return nullptr;
+    }
