03b5c073030b7b03f0d8d40445c037f1f7a7fb5c
hanc2720 <colby.hanna@uleth.ca>
2024-11-25 11:58:32 -0700

    remvoed duplicate void gameLoop

--- include/Game.hpp
+++ include/Game.hpp
@@ -15,10 +15,9 @@ class Game {
  public:
   Game();
   virtual ~Game();
-  void Start();
-  void gameLoop();
   Game(const Game&) = delete;
   Game& operator=(const Game&) = delete;
+  void Start();
   void gameLoop();
   void promptPuzzles(std::vector<std::string> puzzles);
   int userInput(int length);
