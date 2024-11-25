761d3d26e40bafa4f67c15a08dccd9f152c49a95
hanc2720 <colby.hanna@uleth.ca>
2024-11-25 01:34:00 -0700

    Testing if it fixes style check error

--- src/InteractPuzzle.cpp
+++ src/InteractPuzzle.cpp
@@ -16,8 +16,8 @@ puzzle = puz;
 
 void InteractPuzzle::startPuzzle(Player &player,
 std::vector<std::string> &puzzles, bool &changeenv) {
-  if (puzzle != nullptr) {
+  if (puzzle != nullptr) { { // NOLINT(runtime/references)
     delete puzzle;
   }
-  puzzle ->startPuzzle();
+  puzzle ->startPuzzle(player, puzzles, changeenv);
 }
