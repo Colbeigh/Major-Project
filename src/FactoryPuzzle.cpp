5d3d2ae4eb9c64fa89d2e9e9348fd29a06525d52
hanc2720 <colby.hanna@uleth.ca>
2024-11-25 18:46:47 -0700

    Reverted

--- src/FactoryPuzzle.cpp
+++ src/FactoryPuzzle.cpp
@@ -3,8 +3,12 @@
 Puzzle* FactoryPuzzle::createPuzzle(const std::string& type) {
     if (type == "Talk to Ticket Master") {
         return new ticketPuzzle();
+    } else if (type == "Choose where to sit") {
+        return new Sit();
     } else if (type == "Talk to Rich Lady") {
         return new sweetTalkPuzzle();
+    }  else if (type == "Talk to bouncer") {
+        return new Bouncer();
     } else {
         return nullptr;
     }
