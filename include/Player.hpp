0663ef0b09fb6160bf3cd6f060e2c92df4d546f6
hanc2720 <colby.hanna@uleth.ca>
2024-12-07 18:55:28 -0700

    Added comments

--- include/Player.hpp
+++ include/Player.hpp
@@ -12,20 +12,78 @@
 
 class Player {
  public:
+   /**
+   * @brief Sets players name
+   */
   Player();
+
+  /**
+  * @brief Default destructor
+  */
   virtual ~Player();
+
+  /**
+  * @brief Assignment operator to handle copying
+  */
   Player& operator=(const Player& other);
+
+  /**
+  * @brief Returns users name
+  */
   std::string Player::getName();
+
+  /**
+  * @brief Checks if player has the item in their inventory
+  * @param Item thats being checked
+  */
   bool hasItem(std::string itemid);
+
+  /**
+  * @brief Adds Item to players Inventory
+  * @param Item to be added
+  */
   void addItem(std::string itemid);
+
+  /**
+  * @brief Removes Item from players inventory
+  * @param Item to be deleted
+  */
   void remItem(std::string itemid);
+
+  /**
+  * @brief Lists all items in players inventory
+  */
   void listItem();
+
+  /**
+  * @brief Checks if player is alive or dead
+  */
   bool isAlive();
+
+  /**
+  * @brief Sets players Alive
+  */
   void setAlive();
+
+  /**
+  * @brief Sets players Dead
+  */
   void setKill();
 
+
+  /**
+  * @brief Players inventory object
+  */
   Inventory playerinv;
+
+  /**
+  * @brief Players name
+  */
   std::string name;
+
+  /**
+  * @brief Bool for if player is alive or dead
+  */
   bool alive;
 };
 
