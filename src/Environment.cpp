0f63008d75c75396ea17ac6ef64bf102b03ea664
hanc2720 <colby.hanna@uleth.ca>
2024-12-08 19:24:24 -0700

    Updated environment description and help to be more descriptive

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -30,45 +30,50 @@ std::string Environment::getName() {
 PassengerCart::PassengerCart() {
     name = "Passenger Cart";
     environmentDescription = "Looking around the environment, "
-    "you see passeneger, and a ticket master walking around";
-    helpInfo = "You can ask the ticket master for help.";
+    "you see lots of passengers, and a ticket master walking around\n";
+    helpInfo = "Give him your Ticket.\n";
     puzzles = {"Pick a seat"};
 }
 
 
 DiningCart::DiningCart() {
     name = "Dining Cart";
-    environmentDescription = "You are in the dining cart. "
-    "The smell of food fills the air.";
-    helpInfo = "You can ask the chef for help.";
+    environmentDescription = "The smell of food fills the air.\n"
+    "You see a Rich looking lady eating food and a bouncer"
+    " across the cart guarding a door\n";
+    helpInfo = "Talk to the bouncer.";
     puzzles = {"Talk to Rich Lady", "Talk to bouncer"};
 }
 
 GamblingCart::GamblingCart() {
     name = "Gambling Cart";
-    environmentDescription = "You are in the gambling cart.";
-    helpInfo = "You can ask the dealer for help.";
-    puzzles = {"Talk to HelpfullPassenger", "Talk to GambilingOutlaw"};
+    environmentDescription = "It riques of ciggarettes and beer "
+    "You notice a gruff looking dealer and a friendly looking passenger"
+    " smiling at you\n";
+    helpInfo = "You should talk to the friendly looking passenger.\n";
+    puzzles = {"Talk to HelpfulPassenger", "Talk to GambilingOutlaw"};
 }
 
 LuggageCart::LuggageCart() {
     name = "Luggage Cart";
-    environmentDescription = "You are in the luggage cart.";
-    helpInfo = "You can ask the porter for help.";
+    environmentDescription = "Its full of luggage\n";
+    helpInfo = "Look for the luggage on the piece of paper";
     puzzles = {"Catch a breath"};
 }
 
 ArmoryCart::ArmoryCart() {
     name = "Armory Cart";
-    environmentDescription = "You are in the armory cart.";
-    helpInfo = "You can ask the guard for help.";
+    environmentDescription = "Has a heavy open door with metal "
+    "barricades, has a metal rack full of stuff on the top shelf\n";
+    helpInfo = "Think twice before choosing\n";
     puzzles = {"Look around the armory"};
 }
 
 EngineCart::EngineCart() {
     name = "Engine Cart";
-    environmentDescription = "Looking around you see the engine";
-    helpInfo = "You can ask the engineer for help.";
+    environmentDescription = "You see a silhouette and plumes "
+    "of smoke\n";
+    helpInfo = "Remember you have a gun\n";
     puzzles = {"Approach the Gang Leader"};
 }
 
