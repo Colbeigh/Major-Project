488dc885de27eea929bc95f4c3f0fb721dfd4951
pinj2720 <j.pineda@uleth.ca>
2024-12-09 01:12:16 -0700

    fixed some syntax errors

--- test/TestPuzzle.cpp
+++ test/TestPuzzle.cpp
@@ -12,7 +12,7 @@ TEST(PuzzleTest, AddPuzzleTest) {
     puzzle.addPuzzle("puz1");
     puzzle.addPuzzle("puz2");
     std::cout << "adding puzzles\n";
-    EXPECT_EQ(puzzle.puz.size(), 2);
+    EXPECT_EQ(puzzle.puzz.size(), 2);
     EXPECT_EQ(puzzle.puzz[0], "puz1");
     EXPECT_EQ(puzzle.puzz[1], "puz2");
 }
@@ -27,9 +27,9 @@ TEST(PuzzleTest, RemPuzzleTest) {
     }
     puzzle.remPuzzle("puz2");
     std::cout << "removed puz 1\n";
-    EXPECT_EQ(puzzle.puz.size(), 2);
+    EXPECT_EQ(puzzle.puzz.size(), 2);
     EXPECT_EQ(std::find(puzzle.pizz.begin(), puzzle.puzz.end(),
-    "puz2"), puzzle.puzz.end();)
+    "puz2"), puzzle.puzz.end());
     EXPECT_EQ(puzzle.puzz[0], "puz1");
     EXPECT_EQ(puzzle.puzz[1], "puz3");
 }
