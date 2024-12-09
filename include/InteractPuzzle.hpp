313907b9489ed2ed455bf92e2a6aad97fc7200ad
pinj2720 <j.pineda@uleth.ca>
2024-12-08 20:17:41 -0700

    added comments for Interact Puzzle

--- include/InteractPuzzle.hpp
+++ include/InteractPuzzle.hpp
@@ -1,3 +1,7 @@
+/**
+ * @author Jem Pineda <j.pineda@uleth.ca>
+ * @date Fall 2024
+ */
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,12 +15,44 @@
   */
 class InteractPuzzle {
  public:
+
+   /**
+  * @brief Default constructor
+  */
   InteractPuzzle();
+
+  /**
+  * @brief virtual default deconstructor
+  */
   virtual ~InteractPuzzle();
+
+  /**
+  * @brief returns the current puzzle for the environment.
+  * @param puzzle pointer
+  */
   std::vector<std::string> getPuzzle(Puzzle* puzzle);
+
+  /**
+  * @brief returns player and condition
+  * @param puzzle pointer
+  */
   Player getPlayer(Puzzle* puzzle);
+
+  /**
+  * @brief returns bool change enviroment
+  * @param puzzle pointer
+  */
   bool getChangeEnv(Puzzle* puzzle);
+
+  /**
+  * @brief starts puzzle the puzzle. 
+  * @param puz pointer puz
+  * @param player const reference of player
+  * @param puzzles vector string of list of puzzles
+  * @param changeenv bool for changing environment.
+  */
   void startPuzzle(Puzzle* puz, const Player& player,
+
   std::vector<std::string> puzzles, bool changeenv);
 };
 
