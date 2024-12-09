dc14902f965ca498f066886ee646850e009402e0
Nikita Evtushenko <nikita.evtushenk@uleth.ca>
2024-12-08 21:33:22 -0700

    BuildTest Error Fix?

--- test/TestEnvironment.cpp
+++ test/TestEnvironment.cpp
@@ -5,7 +5,7 @@
 TEST(EnvironmentTest, DefaultConstructor) {
     Environment env;
     EXPECT_EQ(env.getDescription(), "You are in an unknown place.");
-    EXPECT_EQ(env.getPuzzles(), {"Empty Puzzle"});
+    EXPECT_EQ(env.getPuzzles(), std::vector<std::string>{"Empty Puzzle"});
     EXPECT_EQ(env.getName(), "Unknowm Place");
     EXPECT_EQ(env.help(), "Help is not implemented yet.");
 }
@@ -15,7 +15,7 @@ TEST(PassenegerCartTest, DefaultConstructor) {
     PassenegerCart cart;
     EXPECT_EQ(cart.getDescription(), "Looking around the environment,"
     "you see passeneger, and a ticket master walking around");
-    EXPECT_EQ(cart.getPuzzles(), {"Pick a seat"});
+    EXPECT_EQ(cart.getPuzzles(), std::vector<std::string>{"Pick a seat"});
     EXPECT_EQ(cart.getName(), "Passenger Cart");
     EXPECT_EQ(cart.help(), "Give him your Ticket.");
 }
@@ -25,7 +25,7 @@ TEST(DiningCartTest, DefaultConstructor) {
     DiningCart cart;
     EXPECT_EQ(cart.getDescription(), "The smell of food fills the air.\n"
     "You see a Rich looking lady eating food and a bouncer");
-    EXPECT_EQ(cart.getPuzzles(), {"Talk to Rich Lady", "Talk to bouncer"});
+    EXPECT_EQ(cart.getPuzzles(), std::vector<std::string>{"Talk to Rich Lady", "Talk to bouncer"});
     EXPECT_EQ(cart.getName(), "Dining Cart");
     EXPECT_EQ(cart.help(), "Talk to the bouncer.");
 }
@@ -36,7 +36,7 @@ TEST(GamblingCartTest, DefaultConstructor) {
     EXPECT_EQ(cart.getDescription(), "It riques of ciggarettes and beer"
     "You notice a gruff looking dealer and a friendly looking passenger"
     "smiling at you\n");
-    EXPECT_EQ(cart.getPuzzles(), {"Talk to HelpfulPassenger",
+    EXPECT_EQ(cart.getPuzzles(), std::vector<std::string>{"Talk to HelpfulPassenger",
     " Talk to GamblingOutlaw"});
     EXPECT_EQ(cart.getName(), "Gambling Cart");
     EXPECT_EQ(cart.help(), "You should talk to the"
@@ -47,7 +47,7 @@ TEST(GamblingCartTest, DefaultConstructor) {
 TEST(LuggageCartTest, DefaultConstructor) {
     LuggageCart cart;
     EXPECT_EQ(cart.getDescription(), "Its full of luggage");
-    EXPECT_EQ(cart.getPuzzles(), {"Catch a breath"});
+    EXPECT_EQ(cart.getPuzzles(), std::vector<std::string>{"Catch a breath"});
     EXPECT_EQ(cart.getName(), "Luggage Cart");
     EXPECT_EQ(cart.help(), "Look for the luggage on the piece of paper");
 }
@@ -57,7 +57,7 @@ TEST(ArmoryCartTest, DefaultConstructor) {
     ArmoryCart cart;
     EXPECT_EQ(cart.getDescription(), "Has a heavy open door with metal "
     "barricades, has a metal rack full of stuff on the top shelf\n");
-    EXPECT_EQ(cart.getPuzzles(), {"Look around the armory"});
+    EXPECT_EQ(cart.getPuzzles(), std::vector<std::string>{"Look around the armory"});
     EXPECT_EQ(cart.getName(), "Armory Cart");
     EXPECT_EQ(cart.help(), "Think twice before choosing\n");
 }
@@ -67,7 +67,7 @@ TEST(EngineCartTest, DefaultConstructor) {
     EngineCart cart;
     EXPECT_EQ(cart.getDescription(), "You see a silhouette and plumes "
     "of smoke\n");
-    EXPECT_EQ(cart.getPuzzles(), {"Approach the Gang Leader"});
+    EXPECT_EQ(cart.getPuzzles(), std::vector<std::string>{"Approach the Gang Leader"});
     EXPECT_EQ(cart.getName(), "Engine Cart");
     EXPECT_EQ(cart.help(), "Remember you have a gun\n");
 }
