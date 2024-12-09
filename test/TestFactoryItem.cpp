2131fdc3901af3e4f2e0d1c9930127733022ab7a
hanc2720 <colby.hanna@uleth.ca>
2024-12-08 23:38:35 -0700

    fixed wrong names

--- test/TestFactoryItem.cpp
+++ test/TestFactoryItem.cpp
@@ -7,23 +7,23 @@ TEST(FactoryItemTest, CreateItem) {
 
     Item* ticketItem = factory.createItem("Ticket");
     EXPECT_NE(ticketItem, nullptr);
-    EXPECT_EQ(ticketItem->getName(), "Ticket");
+    EXPECT_EQ(ticketItem->getName(), "Train Ticket");
 
     Item* punchedTicketItem = factory.createItem("PunchedTicket");
     EXPECT_NE(punchedTicketItem, nullptr);
-    EXPECT_EQ(punchedTicketItem->getName(), "PunchedTicket");
+    EXPECT_EQ(punchedTicketItem->getName(), "Punched Train Ticket");
 
     Item* gamblingTicketItem = factory.createItem("GamblingTicket");
     EXPECT_NE(gamblingTicketItem, nullptr);
-    EXPECT_EQ(gamblingTicketItem->getName(), "GamblingTicket");
+    EXPECT_EQ(gamblingTicketItem->getName(), "Gambling Ticket");
 
     Item* quarterItem = factory.createItem("Quarter");
     EXPECT_NE(quarterItem, nullptr);
-    EXPECT_EQ(quarterItem->getName(), "Quarter");
+    EXPECT_EQ(quarterItem->getName(), "Lucky Quarter");
 
     Item* defaultItem = factory.createItem("InvalidItem");
     EXPECT_NE(defaultItem, nullptr);
-    EXPECT_EQ(defaultItem->getName(), "Ticket");
+    EXPECT_EQ(defaultItem->getName(), "Train Ticket");
 
     delete ticketItem;
     delete punchedTicketItem;
